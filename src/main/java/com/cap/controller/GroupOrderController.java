86d559ad83eda9c59fd653ecd372f86df705ca03
minju-sin <sinminju04@gmail.com>
2023-11-06 16:47:35 +0900

    그룹 주문 생성할 때 호스트와 가게 db에 저장

--- src/main/java/com/cap/controller/GroupOrderController.java
+++ src/main/java/com/cap/controller/GroupOrderController.java
@@ -2,8 +2,9 @@ package com.cap.controller;
 
 import com.cap.domain.GroupOrder;
 import com.cap.domain.User;
+import com.cap.domain.delivery.Store;
 import com.cap.repository.GroupOrderRepository;
-import com.cap.repository.UserRepository;
+import com.cap.repository.StoreRepository;
 import com.cap.service.GroupOrderService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +19,8 @@ public class GroupOrderController {
 
     @Autowired private GroupOrderService groupOrderService;
     @Autowired private GroupOrderRepository groupOrderRepository;
-    @Autowired private UserRepository userRepository;
+    @Autowired private StoreRepository storeRepository;
+
 
     // 'storeId'를 경로 변수로 사용하여 그룹 주문 링크를 생성합니다.
     //  그룹 주문 링크 생성한 사용자 -> 호스트
@@ -37,8 +39,15 @@ public class GroupOrderController {
                 return ResponseEntity.badRequest().body("로그인한 사용자 정보를 가져올 수 없습니다.");
             }
 
-            // 사용자 정보를 이용하여 그룹 주문의 호스트로 설정
-            groupOrder.setOrganizer(loggedInUser);
+            // 가게 정보 가져오기
+            Store store = storeRepository.findById(storeId).orElse(null);
+            if (store == null) {
+                return ResponseEntity.badRequest().body("해당 가게 정보를 찾을 수 없습니다.");
+            }
+
+
+            groupOrder.setOrganizer(loggedInUser);  // 사용자 정보를 이용하여 그룹 주문의 호스트로 설정
+            groupOrder.setStore(store); // 가게 정보 저장
 
             // 그룹 주문 엔티티를 저장합니다.
             groupOrderRepository.save(groupOrder);
@@ -50,4 +59,5 @@ public class GroupOrderController {
             return ResponseEntity.badRequest().body("그룹 주문 링크 생성에 실패했습니다.");
         }
     }
+
 }
