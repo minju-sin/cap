10db45ff5776ec10483557672b2bf16dc2483200
minju-sin <sinminju04@gmail.com>
2023-11-25 12:06:34 +0900

    공지사항 페이지 코드 정리 완성

--- src/main/frontend/src/components/admin/notice/Notice.js
+++ src/main/frontend/src/components/admin/notice/Notice.js
@@ -8,23 +8,10 @@
 * 로그인 전에는 `글쓰기` 버튼 보이지 않도록 구현함
 */
 
-
 import React, { } from 'react';
 import {
-    HomeBody,
-    ContentsText1,
-    ContentsText2,
-    ContentsText3,
-    ContentsText4,
-    Contents,
-    ContentsBox,
-    ContentsImage,
-    StyledLink3, HeaderImage, HeaderText1, HeaderBackgroundColor, HeaderText2, HeaderText3, HeaderText4, HeaderText5
+    HomeBody
 } from "../../HomeCss";
-import menuImage1 from "../../images/ChickenPicture.jpg";
-import menuImage4 from "../../images/PizzaPicture.jpg";
-import menuImage3 from "../../images/Late-night snack picture.jpg";
-import exampleImage from "../../images/HomeHeaderImage.jpg";
 import StyledFooter from "../../style/StyledFooter";
 import StyledHeaderBefore from "../../style/Header/StyledHeaderBefore";
 import StyledLogInBefore from "../../style/Header/StyledLogInBefore";
@@ -34,6 +21,8 @@ import StyledHeaderHome from "../../style/Header/StyledHeaderHome";
 import StyledHeaderAfter from "../../style/Header/StyledHeaderAfter";
 import useAuthStatus from "../../style/Backend/useAuthStatus";
 import StyledNotice from "../../style/Notice/StyledNotice";
+import StyledAi from "../../style/Header/StyledAi";
+import StyledArrow from "../../style/StyledArrow";
 
 function Notice() {
     const {isAuthenticated } = useAuthStatus();
@@ -44,32 +33,12 @@ function Notice() {
                 <HomeBody>
                     <StyledLoginAfter/>
                     <StyledHeaderHome/>
-
-
                     <StyledHeaderAfter/>
-
-                    <ContentsText1>M A T N A M O</ContentsText1>
-                    <ContentsText2>
-                        " 오늘의  &nbsp;<ContentsText3>추천 음식&nbsp;</ContentsText3>{" "}
-                        먹으러 가볼까? "
-                    </ContentsText2>
-                    <Contents>
-                        <ContentsBox>
-                            <ContentsImage src={menuImage1} alt="메뉴 음식 이미지" />
-                            <ContentsText4><StyledLink3 to="/store/category/CHICKEN">치킨</StyledLink3></ContentsText4>
-                        </ContentsBox>
-                        <ContentsBox>
-                            <ContentsImage src={menuImage4} alt="메뉴 음식 이미지" />
-                            <ContentsText4> <StyledLink3 to="/store/category/PIZZA">피자</StyledLink3></ContentsText4>
-                        </ContentsBox>
-                        <ContentsBox>
-                            <ContentsImage src={menuImage3} alt="메뉴 음식 이미지" />
-                            <ContentsText4><StyledLink3 to="/store/category/NIGHT">야식</StyledLink3></ContentsText4>
-                        </ContentsBox>
-                    </Contents>
+                    <StyledAi/>
 
                     <StyledNotice/>
 
+                    <StyledArrow/>
                     <StyledMainPage/>
                     <StyledFooter/>
                 </HomeBody>
@@ -77,12 +46,12 @@ function Notice() {
                 <HomeBody>
                     <StyledLogInBefore/>
                     <StyledHeaderHome/>
-
                     <StyledHeaderBefore/>
-
-
+                    <StyledAi/>
 
                     <StyledNotice/>
+
+                    <StyledArrow/>
                     <StyledMainPage/>
                     <StyledFooter/>
                 </HomeBody>
