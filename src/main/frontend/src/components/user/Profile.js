69c9614915808793686296095021f4aed2784f91
minju-sin <sinminju04@gmail.com>
2023-11-25 13:44:08 +0900

    내정보 페이지 코드 정리

--- src/main/frontend/src/components/user/Profile.js
+++ src/main/frontend/src/components/user/Profile.js
@@ -9,25 +9,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
     HomeBody,
-    Header,
-    Logo,
-    Login,
-    LoginSignUp,
-    StyledLink2,
-    Menu,
-    MenuText,
-    HeaderProImage,
-    HeaderProText,
-    HeaderProButtonImage,
-    HeaderProBox,
-    HeaderProButtonClick,
-    HeaderProBoxSection,
-    ProBox,
-    Hr,
-    StyledLink4,
-    MyproImage,
-    BoxLayout,
-    Hr2,
     HeaderProImage2
 } from "../HomeCss";
 
@@ -50,25 +31,21 @@ import {
     ButtonType2, ButtonFlex
 } from "./ProfileCss"
 
-import proImage1 from "../images/main_pro.png";
-import proButtonImage from "../images/main_pro_button.png";
-import proButtonImageClick from "../images/pro_img_click.png";
-import proImage from "../images/myPro_Image.png";
-import logoutImage from "../images/logout_Image.png";
 import myPageImage from "../images/MyPageImage.png"
 import HomeImage from "../images/HomeImage.png"
 import StyledHeaderHome from "../style/Header/StyledHeaderHome";
 import StyledFooter from "../style/StyledFooter";
+import StyledLoginAfter from "../style/Header/StyledLoginAfter";
+import StyledHeaderAfter from "../style/Header/StyledHeaderAfter";
+import StyledArrow from "../style/StyledArrow";
 
 function Profile() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [ setIsAuthenticated] = useState(false);
     const [userId, setUserId] = useState("");
     const [username, setUsername] = useState("");
     const [isBoxVisible, setBoxVisibility] = useState(true);
 
-    const handleButtonClick = () => {
-        setBoxVisibility(!isBoxVisible);
-    };
+
     useEffect(() => {
         // 서버로 현재 사용자의 인증 상태 확인을 위한 요청 보내기
         axios
@@ -105,21 +82,6 @@ function Profile() {
             });
     }, []);
 
-    // 로그아웃
-    const handleLogout = () => {
-        // 서버의 /logout 엔드포인트로 GET 요청을 보내 로그아웃을 수행
-        axios
-            .get("/logout")
-            .then((response) => {
-                // 로그아웃 성공 시 클라이언트 상태 초기화 및 원하는 작업 수행
-                setIsAuthenticated(false);
-                setUserId("");
-            })
-            .catch((error) => {
-                // 오류 처리
-                console.error("로그아웃 중 오류가 발생했습니다:", error);
-            });
-    };
 
     const [user, setUser] = useState({
         userId: '',
@@ -196,56 +158,10 @@ function Profile() {
 
     return (
         <HomeBody>
-            <Header>
-                <Logo>MatNaMo</Logo>
-                <LoginSignUp>
-                    <Login>
-                        <HeaderProImage src={proImage1} alt="프로필 아이콘 이미지"/>
-                    </Login>
-                    <Login>
-
-                        <HeaderProButtonImage src={proButtonImage} alt="프로필 버튼 이미지"  onClick={handleButtonClick}/>
-                        <HeaderProBox isVisible={isBoxVisible}>
-                            <HeaderProButtonClick src={proButtonImageClick} alt="프로필 클릭시 이미지"/>
-                            <HeaderProBoxSection>
-                                <ProBox>
-                                    <HeaderProImage src={proImage1} alt="프로필 아이콘 이미지"/>
-                                    <HeaderProText>{username}<br/>{userId}</HeaderProText>
-                                </ProBox>
-                                <Hr/>
-                                <BoxLayout>
-                                    <MyproImage src={proImage} alt="내 정보 이미지"/>
-                                    <StyledLink4 to="/profile">내 정보</StyledLink4>
-                                </BoxLayout>
-                                <Hr2/>
-                                <BoxLayout>
-                                    <MyproImage src={logoutImage} alt="로그아웃 이미지"/>
-                                    <StyledLink4 to="/" onClick={handleLogout}>
-                                        로그아웃
-                                    </StyledLink4>
-                                </BoxLayout>
-                            </HeaderProBoxSection>
-                        </HeaderProBox>
-                    </Login>
-                </LoginSignUp>
-            </Header>
-
+            <StyledLoginAfter/>
             <StyledHeaderHome/>
+            <StyledHeaderAfter/>
 
-            <Menu>
-                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;
-                <StyledLink2 to="/board">게시판</StyledLink2>
-                <MenuText>|</MenuText>
-                <StyledLink2 to="/notice">공지사항 </StyledLink2>
-                <MenuText>|</MenuText>
-                {userId === "admin" ? (
-                    // 관리자 메인 화면 페이지
-                    <StyledLink2 to="/management">사용자 관리</StyledLink2>
-                ) : (
-                    // 사용자 메인 화면 페이지
-                    <StyledLink2 to="/profile">내 정보</StyledLink2>
-                )}
-            </Menu>
             <MyPageFlex>
                 <MyPageFlex_2>
                     <MypageFont>MYPAGE</MypageFont>
@@ -299,7 +215,7 @@ function Profile() {
                             {isEditing ? (
                                 <>
                                     <MyPageInputType2 type="text" value={selectedAddress} readOnly />
-                                    <ButtonType1 type="button" onClick={openAddressSearch}>도로명 검색</ButtonType1>
+                                    <ButtonType1 type="button" onClick={openAddressSearch}>검색</ButtonType1>
                                 </>
                             ) : (
                                 <MyPageInputType type="text" value={user.address} readOnly />
@@ -324,6 +240,8 @@ function Profile() {
                     </FromStyle>
                 </MyPageFlex_3>
             </MyPageFlex>
+
+            <StyledArrow/>
             <StyledFooter/>
         </HomeBody>
 
