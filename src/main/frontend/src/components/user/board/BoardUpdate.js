c706d5d24a74a03e07624013f37cc818ff2a2b4e
minju-sin <sinminju04@gmail.com>
2023-11-25 13:23:42 +0900

    1차 코드 정리 게시글 수정 페이지

--- src/main/frontend/src/components/user/board/BoardUpdate.js
+++ src/main/frontend/src/components/user/board/BoardUpdate.js
@@ -17,44 +17,15 @@ import {
 } from "../../admin/notice/NoticeDetailCss";
 
 import {
-    HomeBody,
-    Header,
-    Logo,
-    Login,
-    LoginSignUp,
-    StyledLink2,
-    HeaderImage,
-    HeaderText1,
-    HeaderText2,
-    HeaderBackgroundColor,
-    HeaderText3,
-    HeaderText4,
-    HeaderText5,
-    Menu,
-    MenuText,
-    HeaderProImage,
-    HeaderProText,
-    HeaderProButtonImage,
-    HeaderProBox,
-    HeaderProButtonClick,
-    HeaderProBoxSection,
-    ProBox,
-    Hr,
-    StyledLink4,
-    MyproImage,
-    BoxLayout,
-    Hr2,
-    HomeLogoImage
+    HomeBody
 } from "../../HomeCss";
 
-import exampleImage from "../../images/HomeHeaderImage.jpg";
-import proImage1 from "../../images/main_pro.png";
-import proButtonImage from "../../images/main_pro_button.png";
-import proButtonImageClick from "../../images/pro_img_click.png";
-import proImage from "../../images/myPro_Image.png"
-import logoutImage from "../../images/logout_Image.png"
 import Swal from "sweetalert2";
 import StyledFooter from "../../style/StyledFooter";
+import StyledLoginAfter from "../../style/Header/StyledLoginAfter";
+import StyledHeaderHome from "../../style/Header/StyledHeaderHome";
+import StyledHeaderAfter from "../../style/Header/StyledHeaderAfter";
+import StyledArrow from "../../style/StyledArrow";
 
 function BoardUpdate() {
     const { articleId } = useParams(); // URL에서 articleId를 가져옴
@@ -62,11 +33,6 @@ function BoardUpdate() {
     const [content, setContent] = useState('');
     const [orderLink, setOrderLink] = useState('');
     const [address, setAddress] = useState('');
-
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [userId, setUserId] = useState("");
-    const [username, setUsername] = useState("");
-    const [isBoxVisible, setBoxVisibility] = useState(true);
 
     const [title1, setTitle1] = useState("");
     const [section, setSection] = useState("");
@@ -88,62 +54,6 @@ function BoardUpdate() {
             setErrorSection("");
         }
     }
-
-    const handleButtonClick = () => {
-        setBoxVisibility(!isBoxVisible);
-    };
-
-    useEffect(() => {
-        // 서버로 현재 사용자의 인증 상태 확인을 위한 요청 보내기
-        axios
-            .get("/check-auth")
-            .then((response) => {
-                if (response.data === "authenticated") {
-                    setIsAuthenticated(true);
-
-                    // 사용자 ID를 가져와 상태에 저장
-                    axios
-                        .get("/get-user-id")
-                        .then((response) => {
-                            setUserId(response.data);
-                        })
-                        .catch((error) => {
-                            // 에러 처리
-                        });
-
-                    // 사용자 ID를 가져와 상태에 저장
-                    axios
-                        .get("/get-user-name")
-                        .then((response) => {
-                            setUsername(response.data);
-                        })
-                        .catch((error) => {
-                            // 에러 처리
-                        });
-                } else {
-                    setIsAuthenticated(false);
-                }
-            })
-            .catch((error) => {
-                // 요청 실패 처리
-            });
-    }, []);
-
-    // 로그아웃
-    const handleLogout = () => {
-        // 서버의 /logout 엔드포인트로 GET 요청을 보내 로그아웃을 수행
-        axios
-            .get("/logout")
-            .then((response) => {
-                // 로그아웃 성공 시 클라이언트 상태 초기화 및 원하는 작업 수행
-                setIsAuthenticated(false);
-                setUserId("");
-            })
-            .catch((error) => {
-                // 오류 처리
-                console.error("로그아웃 중 오류가 발생했습니다:", error);
-            });
-    };
 
     //  주소 API
     const openAddressSearch = () => {
@@ -218,86 +128,9 @@ function BoardUpdate() {
 
     return (
         <HomeBody>
-            <Header>
-                <HomeLogoImage>
-                    {/*<LogoImage2 src={logoImage2} alt="프로필 아이콘 이미지"/>*/}
-                    <Logo>MatNaMo</Logo>
-                </HomeLogoImage>
-                <LoginSignUp>
-                    <Login>
-                        <HeaderProImage src={proImage1} alt="프로필 아이콘 이미지"/>
-                    </Login>
-                    <Login>
-
-                        <HeaderProButtonImage src={proButtonImage} alt="프로필 버튼 이미지"  onClick={handleButtonClick}/>
-                        <HeaderProBox isVisible={isBoxVisible}>
-                            <HeaderProButtonClick src={proButtonImageClick} alt="프로필 클릭시 이미지"/>
-                            <HeaderProBoxSection>
-                                <ProBox>
-                                    <HeaderProImage src={proImage1} alt="프로필 아이콘 이미지"/>
-                                    <HeaderProText>{username}<br/>{userId}</HeaderProText>
-                                </ProBox>
-                                <Hr/>
-                                <BoxLayout>
-                                    <MyproImage src={proImage} alt="내 정보 이미지"/>
-                                    {userId === "admin" ? (
-                                        // 관리자 메인 화면 페이지
-                                        <StyledLink4 to="/management">사용자 관리</StyledLink4>
-                                    ) : (
-                                        // 사용자 메인 화면 페이지
-                                        <StyledLink4 to="/profile">내 정보</StyledLink4>
-                                    )}
-                                </BoxLayout>
-                                <Hr2/>
-                                <BoxLayout>
-                                    <MyproImage src={logoutImage} alt="로그아웃 이미지"/>
-                                    <StyledLink4 to="/" onClick={handleLogout}>
-                                        로그아웃
-                                    </StyledLink4>
-                                </BoxLayout>
-
-
-                            </HeaderProBoxSection>
-                        </HeaderProBox>
-                    </Login>
-                </LoginSignUp>
-            </Header>
-
-            <HeaderImage src={exampleImage} alt="헤더 배경 이미지" />
-
-            <HeaderText1>
-                <HeaderBackgroundColor></HeaderBackgroundColor>
-                <HeaderText2>" MatNaMo "</HeaderText2>
-                <HeaderText3>
-                    <HeaderText4>
-                        <HeaderText5>맛나모( MatNaMo )</HeaderText5>는 "맛있는
-                        나눔(Mate)"을 의미하며,
-                    </HeaderText4>
-                    <HeaderText4>
-                        학생들 간의 음식 나눔을 촉진하는 메시지를 전달합니다.
-                    </HeaderText4>
-                    <HeaderText4>
-                        이 플랫폼은 음식 공동 주문을 통해{" "}
-                        <HeaderText5>배달비와 주문최소금액</HeaderText5>을 절감 할 수
-                        있습니다.
-                    </HeaderText4>
-                </HeaderText3>
-            </HeaderText1>
-
-            <Menu>
-                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;
-                <StyledLink2 to="/board">게시판</StyledLink2>
-                <MenuText>|</MenuText>
-                <StyledLink2 to="/notice">공지사항 </StyledLink2>
-                <MenuText>|</MenuText>
-                {userId === "admin" ? (
-                    // 관리자 메인 화면 페이지
-                    <StyledLink2 to="/management">사용자 관리</StyledLink2>
-                ) : (
-                    // 사용자 메인 화면 페이지
-                    <StyledLink2 to="/profile">내 정보</StyledLink2>
-                )}
-            </Menu>
+            <StyledLoginAfter/>
+            <StyledHeaderHome/>
+            <StyledHeaderAfter/>
 
             <BodyWrapper>
                 <HeaderFont>공지사항 수정</HeaderFont>
@@ -389,6 +222,7 @@ function BoardUpdate() {
                 <Button_2  onClick={handleSubmit}>수정 하기</Button_2>
             </BodyWrapper>
 
+            <StyledArrow/>
             <StyledFooter/>
         </HomeBody>
     );
