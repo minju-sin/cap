cbd89797954e2066ee276a50953ed0a7fc06cde5
minju-sin <sinminju04@gmail.com>
2023-11-25 14:28:36 +0900

    가게 상세 페이지 코드 정리 1차

--- src/main/frontend/src/components/delivery/StoreDetail.js
+++ src/main/frontend/src/components/delivery/StoreDetail.js
@@ -123,6 +123,14 @@ import {
 import BoardShowImage3 from "../images/BoardShowImage3.png";
 import {HomeImageCss, LinkButtonFont1, MainPageFlex, MypageFont3} from "../user/ProfileCss";
 import HomeImage from "../images/HomeImage.png";
+import StyledFooter from "../style/StyledFooter";
+import StyledArrow from "../style/StyledArrow";
+import StyledMainPage from "../style/StyledMainPage";
+import StyledHeaderBefore from "../style/Header/StyledHeaderBefore";
+import StyledLoginBefore from "../style/Header/StyledLogInBefore";
+import StyledHeaderHome from "../style/Header/StyledHeaderHome";
+import StyledLoginAfter from "../style/Header/StyledLoginAfter";
+import StyledHeaderAfter from "../style/Header/StyledHeaderAfter";
 
 
 // 스타일 태그 내의 CSS - 모달창 디자인
@@ -206,16 +214,13 @@ function formatNumberWithCommas(number) {
 
 function StoreDetail({ match }) {
     const [isAuthenticated, setIsAuthenticated] = useState(false); // 사용자 로그인 상태
+    
     const [menus, setMenus] = useState([]);
     const [showModal, setShowModal] = useState(false); // 모달 상태 변수 추가
     const [selectedMenu, setSelectedMenu] = useState(null); // 선택된 메뉴 정보 (모달창으로 보여줌)
     const { storeId } = useParams();
     const [groupOrderUrl, setGroupOrderUrl] = useState(''); // 그룹 주문 URL 상태
     const [articles, setArticles] = useState([]);
-
-    const [userId, setUserId] = useState("");
-    const [username, setUsername] = useState("");
-    const [isBoxVisible, setBoxVisibility] = useState(true);
 
     const [search, setSearch] = useState(''); // 검색어 상태 추가
 
@@ -249,62 +254,6 @@ function StoreDetail({ match }) {
             })
             .catch(error => {
                 console.error('게시글 검색 중 오류가 발생했습니다:', error);
-            });
-    };
-
-    const handleButtonClick = () => {
-        setBoxVisibility(!isBoxVisible);
-    };
-
-    useEffect(() => {
-        // 서버로 현재 사용자의 인증 상태 확인을 위한 요청 보내기
-        axios
-            .get("/check-auth")
-            .then((response) => {
-                if (response.data === "authenticated") {
-                    setIsAuthenticated(true);
-
-                    // 사용자 ID를 가져와 상태에 저장
-                    axios
-                        .get("/get-user-id")
-                        .then((response) => {
-                            setUserId(response.data);
-                        })
-                        .catch((error) => {
-                            // 에러 처리
-                        });
-
-                    // 사용자 ID를 가져와 상태에 저장
-                    axios
-                        .get("/get-user-name")
-                        .then((response) => {
-                            setUsername(response.data);
-                        })
-                        .catch((error) => {
-                            // 에러 처리
-                        });
-                } else {
-                    setIsAuthenticated(false);
-                }
-            })
-            .catch((error) => {
-                // 요청 실패 처리
-            });
-    }, []);
-
-    // 로그아웃
-    const handleLogout = () => {
-        // 서버의 /logout 엔드포인트로 GET 요청을 보내 로그아웃을 수행
-        axios
-            .get("/logout")
-            .then((response) => {
-                // 로그아웃 성공 시 클라이언트 상태 초기화 및 원하는 작업 수행
-                setIsAuthenticated(false);
-                setUserId("");
-            })
-            .catch((error) => {
-                // 오류 처리
-                console.error("로그아웃 중 오류가 발생했습니다:", error);
             });
     };
 
@@ -382,85 +331,10 @@ function StoreDetail({ match }) {
         <><style>{modalStyle}</style>
             {isAuthenticated ? (
                 <HomeBody>
-                    <Header>
-                        <HomeLogoImage>
-                            {/*<LogoImage2 src={logoImage2} alt="프로필 아이콘 이미지"/>*/}
-                            <Logo>MatNaMo</Logo>
-                        </HomeLogoImage>
-                        <LoginSignUp>
-                            <Login>
-                                <HeaderProImage src={proImage1} alt="프로필 아이콘 이미지"/>
-                            </Login>
-                            <Login>
-                                <HeaderProButtonImage src={proButtonImage} alt="프로필 버튼 이미지"  onClick={handleButtonClick}/>
-                                <HeaderProBox isVisible={isBoxVisible}>
-                                    <HeaderProButtonClick src={proButtonImageClick} alt="프로필 클릭시 이미지"/>
-                                    <HeaderProBoxSection>
-                                        <ProBox>
-                                            <HeaderProImage src={proImage1} alt="프로필 아이콘 이미지"/>
-                                            <HeaderProText>{username}<br/>{userId}</HeaderProText>
-                                        </ProBox>
-                                        <Hr/>
-                                        <BoxLayout>
-                                            <MyproImage src={proImage} alt="내 정보 이미지"/>
-                                            {userId === "admin" ? (
-                                                // 관리자 메인 화면 페이지
-                                                <StyledLink4 to="/management">사용자 관리</StyledLink4>
-                                            ) : (
-                                                // 사용자 메인 화면 페이지
-                                                <StyledLink4 to="/profile">내 정보</StyledLink4>
-                                            )}
-                                        </BoxLayout>
-                                        <Hr2/>
-                                        <BoxLayout>
-                                            <MyproImage src={logoutImage} alt="내 정보 이미지"/>
-                                            <StyledLink4 to="/" onClick={handleLogout}>
-                                                로그아웃
-                                            </StyledLink4>
-                                        </BoxLayout>
+                    <StyledLoginAfter/>
+                    <StyledHeaderHome/>
+                    <StyledHeaderAfter/>
 
-
-                                    </HeaderProBoxSection>
-                                </HeaderProBox>
-                            </Login>
-                        </LoginSignUp>
-                    </Header>
-
-                    <HeaderImage src={exampleImage} alt="헤더 배경 이미지" />
-
-                    <HeaderText1>
-                        <HeaderBackgroundColor></HeaderBackgroundColor>
-                        <HeaderText2>" MatNaMo "</HeaderText2>
-                        <HeaderText3>
-                            <HeaderText4>
-                                <HeaderText5>맛나모( MatNaMo )</HeaderText5>는 "맛있는
-                                나눔(Mate)"을 의미하며,
-                            </HeaderText4>
-                            <HeaderText4>
-                                학생들 간의 음식 나눔을 촉진하는 메시지를 전달합니다.
-                            </HeaderText4>
-                            <HeaderText4>
-                                이 플랫폼은 음식 공동 주문을 통해{" "}
-                                <HeaderText5>배달비와 주문최소금액</HeaderText5>을 절감 할 수
-                                있습니다.
-                            </HeaderText4>
-                        </HeaderText3>
-                    </HeaderText1>
-
-                    <Menu>
-                        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;
-                        <StyledLink2 to="/board">게시판</StyledLink2>
-                        <MenuText>l</MenuText>
-                        <StyledLink2 to="/notice">공지사항 </StyledLink2>
-                        <MenuText>l</MenuText>
-                        {userId === "admin" ? (
-                            // 관리자 메인 화면 페이지
-                            <StyledLink2 to="/management">사용자 관리</StyledLink2>
-                        ) : (
-                            // 사용자 메인 화면 페이지
-                            <StyledLink2 to="/profile">내 정보</StyledLink2>
-                        )}
-                    </Menu>
                     <StoreDetailBody2>
                         <StoreDetailBody>
                             <StoreDetailStore>
@@ -621,12 +495,8 @@ function StoreDetail({ match }) {
                                 </Link>
                             )}
 
-                            <LinkButtonFont1 to={`/`}>
-                                <MainPageFlex>
-                                    <HomeImageCss src={HomeImage} alt="홈 이미지"/>
-                                    <MypageFont3>메인 홈페이지</MypageFont3>
-                                </MainPageFlex>
-                            </LinkButtonFont1>
+                            <StyledMainPage/>
+
                         </StoreDetailBody>
                         <StoreDetailBarFlex>
                             <StoreDetailBarAll>
@@ -654,73 +524,15 @@ function StoreDetail({ match }) {
                         </StoreDetailBarFlex>
                     </StoreDetailBody2>
 
-                    <Footer>
-                        <Footer1>
-                            <FooterText>MatNaMo</FooterText>
-                            <FooterText2>이성민(팀장) : 프로젝트 총괄 기획, 웹 퍼블리셔, 프론트엔드</FooterText2>
-                            <FooterText2>우가현(팀원) : 웹 퍼블리셔</FooterText2>
-                            <FooterText2>신민주(팀원) : 백엔드, DB설계</FooterText2>
-                            <FooterText2>이지훈(팀원) : 웹 크롤링, 인공지능</FooterText2>
-
-                            <FooterImages>
-                                <FooterImage src={facebookImage} alt="페이스북 이미지"></FooterImage>
-                                <FooterImage src={instagramImage} alt="인스타그램 이미지"></FooterImage>
-                                <FooterImage src={youtubeImage} alt ="유튜브 이미지"></FooterImage>
-                            </FooterImages>
-                            <Hr2></Hr2>
-                            <FooterText2>@2023 Capstone Project MatNaMo</FooterText2>
-                        </Footer1>
-                    </Footer>
+                    <StyledArrow/>
+                    <StyledFooter/>
                 </HomeBody>
             ) : (
                 <HomeBody>
-                    <Header>
-                        <Logo>MatNaMo</Logo>
-                        <LoginSignUp>
-                            <Login>
-                                <StyledLink1 to="/login">로그인</StyledLink1>
-                            </Login>
-                            <SignUp>
-                                <StyledLink1 to="/signup">회원가입</StyledLink1>
-                            </SignUp>
-                        </LoginSignUp>
-                    </Header>
+                    <StyledLoginBefore/>
+                    <StyledHeaderHome/>
+                    <StyledHeaderBefore/>
 
-                    <HeaderImage src={exampleImage} alt="헤더 배경 이미지" />
-
-                    <HeaderText1>
-                        <HeaderBackgroundColor></HeaderBackgroundColor>
-                        <HeaderText2>" MatNaMo "</HeaderText2>
-                        <HeaderText3>
-                            <HeaderText4>
-                                <HeaderText5>맛나모( MatNaMo )</HeaderText5>는 "맛있는
-                                나눔(Mate)"을 의미하며,
-                            </HeaderText4>
-                            <HeaderText4>
-                                학생들 간의 음식 나눔을 촉진하는 메시지를 전달합니다.
-                            </HeaderText4>
-                            <HeaderText4>
-                                이 플랫폼은 음식 공동 주문을 통해{" "}
-                                <HeaderText5>배달비와 주문최소금액</HeaderText5>을 절감 할 수
-                                있습니다.
-                            </HeaderText4>
-                        </HeaderText3>
-                    </HeaderText1>
-
-                    <Menu>
-                        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;
-                        <StyledLink2 to="/board">게시판</StyledLink2>
-                        <MenuText>l</MenuText>
-                        <StyledLink2 to="/notice">공지사항 </StyledLink2>
-                        <MenuText>l</MenuText>
-                        {userId === "admin" ? (
-                            // 관리자 메인 화면 페이지
-                            <StyledLink2 to="/management">사용자 관리</StyledLink2>
-                        ) : (
-                            // 사용자 메인 화면 페이지
-                            <StyledLink2 to="/profile">내 정보</StyledLink2>
-                        )}
-                    </Menu>
                     <StoreDetailBody2>
                         <StoreDetailBody>
                             <StoreDetailStore>
@@ -880,12 +692,7 @@ function StoreDetail({ match }) {
                                 </Link>
                             )}
 
-                            <LinkButtonFont1 to={`/`}>
-                                <MainPageFlex>
-                                    <HomeImageCss src={HomeImage} alt="홈 이미지"/>
-                                    <MypageFont3>메인 홈페이지</MypageFont3>
-                                </MainPageFlex>
-                            </LinkButtonFont1>
+                            <StyledMainPage/>
                         </StoreDetailBody>
 
                         <StoreDetailBarFlex>
@@ -914,23 +721,8 @@ function StoreDetail({ match }) {
                         </StoreDetailBarFlex>
                     </StoreDetailBody2>
 
-                    <Footer>
-                        <Footer1>
-                            <FooterText>MatNaMo</FooterText>
-                            <FooterText2>이성민(팀장) : 프로젝트 총괄 기획, 웹 퍼블리셔, 프론트엔드</FooterText2>
-                            <FooterText2>우가현(팀원) : 웹 퍼블리셔</FooterText2>
-                            <FooterText2>신민주(팀원) : 백엔드, DB설계</FooterText2>
-                            <FooterText2>이지훈(팀원) : 웹 크롤링, 인공지능</FooterText2>
-
-                            <FooterImages>
-                                <FooterImage src={facebookImage} alt="페이스북 이미지"></FooterImage>
-                                <FooterImage src={instagramImage} alt="인스타그램 이미지"></FooterImage>
-                                <FooterImage src={youtubeImage} alt ="유튜브 이미지"></FooterImage>
-                            </FooterImages>
-                            <Hr2></Hr2>
-                            <FooterText2>@2023 Capstone Project MatNaMo</FooterText2>
-                        </Footer1>
-                    </Footer>
+                    <StyledArrow/>
+                    <StyledFooter/>
                 </HomeBody>
             )}
         </>
