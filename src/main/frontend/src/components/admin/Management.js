c8adb6a5509d3cef36e06c0ab8201408a39cfe7f
minju-sin <sinminju04@gmail.com>
2023-11-25 12:45:26 +0900

    코드 정리

--- src/main/frontend/src/components/admin/Management.js
+++ src/main/frontend/src/components/admin/Management.js
@@ -7,41 +7,8 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Link} from "react-router-dom";
 import {
-    HomeBody,
-    Header,
-    Logo,
-    Login,
-    LoginSignUp,
-    StyledLink2,
-    HeaderImage,
-    HeaderText1,
-    HeaderText2,
-    HeaderBackgroundColor,
-    HeaderText3,
-    HeaderText4,
-    HeaderText5,
-    Menu,
-    MenuText,
-    HeaderProImage,
-    HeaderProText,
-    HeaderProButtonImage,
-    HeaderProBox,
-    HeaderProButtonClick,
-    HeaderProBoxSection,
-    ProBox,
-    Hr,
-    StyledLink4,
-    MyproImage,
-    BoxLayout,
-    Hr2,
-    Footer,
-    FooterText,
-    FooterText2,
-    FooterImage,
-    FooterImages,
-    Footer1, HeaderProImage2
+    HomeBody
 } from "../HomeCss"
 
 import {
@@ -52,23 +19,6 @@ import {
 
 } from "./ManagementCss"
 
-
-import exampleImage from "../images/HomeHeaderImage.jpg";
-import proImage1 from "../images/main_pro.png";
-import proButtonImage from "../images/main_pro_button.png";
-import proButtonImageClick from "../images/pro_img_click.png";
-import proImage from "../images/myPro_Image.png";
-import logoutImage from "../images/logout_Image.png";
-import facebookImage from "../images/facebookImage.png";
-import instagramImage from "../images/Instagram.png";
-import youtubeImage from "../images/Youtube.png";
-import {
-    HomeImageCss,
-    MainPageFlex,
-    MypageFont3,
-} from "../user/ProfileCss";
-import HomeImage from "../images/HomeImage.png";
-import myPageImage from "../images/MyPageImage.png";
 import StyledLoginAfter from "../style/Header/StyledLoginAfter";
 import StyledHeaderHome from "../style/Header/StyledHeaderHome";
 import StyledHeaderAfter from "../style/Header/StyledHeaderAfter";
@@ -77,15 +27,8 @@ import StyledArrow from "../style/StyledArrow";
 import StyledFooter from "../style/StyledFooter";
 
 function Management() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [userId, setUserId] = useState("");
-    const [username, setUsername] = useState("");
-    const [isBoxVisible, setBoxVisibility] = useState(true);
     const [users, setUsers] = useState([]);
 
-    const handleButtonClick = () => {
-        setBoxVisibility(!isBoxVisible);
-    };
 
     useEffect(() => {
         // 서버로부터 사용자 목록을 가져오는 요청을 보냅니다.
@@ -95,38 +38,6 @@ function Management() {
             })
             .catch(error => {
                 console.error('사용자 목록을 불러오는 중 오류가 발생했습니다:', error);
-            });
-
-        // 서버로 현재 사용자의 인증 상태 확인을 위한 요청 보내기
-        axios.get("/check-auth")
-            .then((response) => {
-                if (response.data === "authenticated") {
-                    setIsAuthenticated(true);
-
-                    // 사용자 ID를 가져와 상태에 저장
-                    axios
-                        .get("/get-user-id")
-                        .then((response) => {
-                            setUserId(response.data);
-                        })
-                        .catch((error) => {
-                            // 에러 처리
-                        });
-
-                    // 사용자 ID를 가져와 상태에 저장
-                    axios.get("/get-user-name")
-                        .then((response) => {
-                            setUsername(response.data);
-                        })
-                        .catch((error) => {
-                            // 에러 처리
-                        });
-                } else {
-                    setIsAuthenticated(false);
-                }
-            })
-            .catch((error) => {
-                // 요청 실패 처리
             });
     }, []);
 
@@ -147,20 +58,6 @@ function Management() {
             });
     };
 
-    // 로그아웃
-    const handleLogout = () => {
-        // 서버의 /logout 엔드포인트로 GET 요청을 보내 로그아웃을 수행
-        axios.get("/logout")
-            .then((response) => {
-                // 로그아웃 성공 시 클라이언트 상태 초기화 및 원하는 작업 수행
-                setIsAuthenticated(false);
-                setUserId("");
-            })
-            .catch((error) => {
-                // 오류 처리
-                console.error("로그아웃 중 오류가 발생했습니다:", error);
-            });
-    };
 
     return (
         <HomeBody>
