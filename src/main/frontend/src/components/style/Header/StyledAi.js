82bf36ae27be6a3a1808c8c5be04ae28e5a4a0e7
minju-sin <sinminju04@gmail.com>
2023-12-04 12:05:33 +0900

    추천음식 프론트 수정

--- src/main/frontend/src/components/style/Header/StyledAi.js
+++ src/main/frontend/src/components/style/Header/StyledAi.js
@@ -1,17 +1,12 @@
 import {
     Contents,
-    ContentsBox, ContentsBoxLink,
+    ContentsBox,
     ContentsImage,
     ContentsText1,
     ContentsText2,
-    ContentsText3,
-    ContentsText4,
-    StyledLink3
+    ContentsText3
 } from "../../HomeCss";
 import React from "react";
-import menuImage1 from "../../images/ChickenPicture.jpg";
-import menuImage4 from "../../images/PizzaPicture.jpg";
-import menuImage3 from "../../images/Late-night snack picture.jpg";
 import machine_learning_food from "../../images/recommend_food.jpg";
 
 const StyledAi = ({ }) => {
@@ -23,24 +18,9 @@ const StyledAi = ({ }) => {
                 먹으러 가볼까? "
             </ContentsText2>
             <Contents>
-                <ContentsBoxLink to="/store/category/CHICKEN">
-                    <ContentsBox>
-                        <ContentsImage src={menuImage1} alt="메뉴 음식 이미지" />
-                        <ContentsText4><StyledLink3 to="/store/category/CHICKEN">치킨</StyledLink3></ContentsText4>
-                    </ContentsBox>
-                </ContentsBoxLink>
-                <ContentsBoxLink>
                 <ContentsBox>
                     <ContentsImage src={machine_learning_food} alt="인공지능 추천 음식 이미지" />
-                    <ContentsText4>AI 추천 음식</ContentsText4>
                 </ContentsBox>
-                </ContentsBoxLink>
-                <ContentsBoxLink to="/store/category/NIGHT">
-                <ContentsBox>
-                    <ContentsImage src={menuImage3} alt="메뉴 음식 이미지" />
-                    <ContentsText4><StyledLink3 to="/store/category/NIGHT">야식</StyledLink3></ContentsText4>
-                </ContentsBox>
-                </ContentsBoxLink>
             </Contents>
         </>
     );
